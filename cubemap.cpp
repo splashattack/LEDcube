fd223c53b4243ad17dc9b5c29179f2f9233ba33f
Joseph Leeper <jl933a@lab.icc.edu>
2014-03-09 21:52:10 -0500

    Removal of STL dependency.

--- cubemap.cpp
+++ cubemap.cpp
@@ -1,8 +1,20 @@
 #include "cubemap.h"
 
-const byte LAYERS = 4;
-
-// void cubemap::updateLayer(const bitset& pattern) const
+// cubemap::cubemap()
 // {
-//     frame[pattern[0]] = pattern.to_ulong & 0xFFFF;
+    
 // }
+
+void cubemap::updateLayer(const unsigned long& input_bits)
+{
+    byte select = (byte) (input_bits & 0xFFFF0000) >> 16;
+    unsigned int pattern = input_bits & 0xFFFF;
+    if (select == 0)
+    {
+        delay(pattern);
+    }
+    else
+    {
+        frame[select] = pattern;
+    }
+}
--- cubemap.h
+++ cubemap.h
@@ -1,19 +1,18 @@
 #ifndef CUBEMAP_H
 #define CUBEMAP_H
 
-#include <bitset>
-//#include <Arduino.h>
+#include <Arduino.h>
 
 class cubemap
 {
-private:
-    //std::bitset<16> frame[LAYERS];
-    std::bitset<16> foo;
-    std::bitset<16> bar (0xfa2);
-    std::bitset<16> baz (std::string("0101111001"));;
 
 public:
-    void updateLayer(const bitset& pattern) const;
+    static const byte LAYERS = 4;
+    void updateLayer(const unsigned long& pattern);
+
+private:
+    unsigned long frame[LAYERS];
+
 };
 
 
