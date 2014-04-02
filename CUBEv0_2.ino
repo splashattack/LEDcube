7e885f5aa9a5a4b13dc1f8b1eed65b3fa2068d55
Joseph Leeper <jl933a@lab.icc.edu>
2014-04-02 16:27:34 -0500

    ISR now triggers at 100 Hz, meaning no layer should be off for more than 40 ms.

--- CUBEv0_2.ino
+++ CUBEv0_2.ino
@@ -54,12 +54,12 @@ void setup()
   TCCR1A = 0;     // set entire TCCR1A register to 0
   TCCR1B = 0;     // same for TCCR1B
  
-  // set compare match register to desired timer count:
-  OCR1A = 15624;
+  // set compare match register to 100 Hz @ 256 prescaler:
+  OCR1A = 624;
   // turn on CTC mode:
   TCCR1B |= (1 << WGM12);
   // Set CS10 and CS12 bits for 1024 prescaler:
-  TCCR1B |= (1 << CS10);
+  //TCCR1B |= (1 << CS10);
   TCCR1B |= (1 << CS12);
   // enable timer compare interrupt:
   TIMSK1 |= (1 << OCIE1A);
